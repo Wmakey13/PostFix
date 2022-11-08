43e65ac8ecb72ca2f1c2b71d5cfbd819f488911d
Warren <WK052226@cerner.net>
2022-11-08 00:48:57 +0000

    PostFixProgram Condense into one class

--- src/main/PostFixProgram.java
+++ src/main/PostFixProgram.java
@@ -1,14 +1,16 @@
 package main;
 
-
-import java.sql.Array;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Stack;
 
 public class PostFixProgram
 {
-    CSVParser csvParser;
-    static Calculator calculator = new Calculator();
     static String[][] equationsByCell;
     public static String[][] results;
 
@@ -31,7 +33,7 @@ public class PostFixProgram
     {
         try
         {
-            equationsByCell = new CSVParser().parseFile(csvFile);
+            equationsByCell = parseFile(csvFile);
         }
         catch (Exception e)
         {
@@ -50,7 +52,7 @@ public class PostFixProgram
             {
                 if (equationsByCell[row][column] != null)
                 {
-                    results[row][column] = calculator.calculate(prepareString(equationsByCell[row][column]));
+                    results[row][column] = calculate(prepareString(equationsByCell[row][column]));
                 }
             }
         }
@@ -61,7 +63,7 @@ public class PostFixProgram
             {
                 if (results[row][column] != null && !results[row][column].matches("[0-9]+"))
                 {
-                    results[row][column] = calculator.calculate(prepareString(equationsByCell[row][column]));
+                    results[row][column] = calculate(prepareString(equationsByCell[row][column]));
                 }
             }
         }
@@ -90,6 +92,93 @@ public class PostFixProgram
             }
         }
         return preparedString.toString();
+    }
+
+    public static String calculate(String string)
+    {
+        String[] items = string.split(" ");
+        Stack<Float> values = new Stack<>();
+        for (String item : items)
+        {
+            if (item.equals("+") || item.equals("/") || item.equals("*") || item.equals("-"))
+            {
+                if (values.size() < 2)
+                {
+                    return "#ERR";
+                }
+                float a = values.pop();
+                float b = values.pop();
+                values.push(calculateItems(a, b, item));
+            }
+            else
+            {
+                try
+                {
+                    values.add(Float.parseFloat(item));
+                }
+                catch (Exception e)
+                {
+                    return "#ERR";
+                }
+            }
+        }
+        if (values.size() > 1)
+        {
+            return "#ERR";
+        }
+
+        BigDecimal total = new BigDecimal(Double.toString(values.pop()));
+        total = total.setScale(1, RoundingMode.HALF_UP);
+        return total.doubleValue() % 1 == 0 ? String.format("%.0f", total) : total.toString();
+    }
+
+    public static float calculateItems(float a, float b, String c)
+    {
+        switch (c)
+        {
+            case "+":
+                return b + a;
+            case "-":
+                return b - a;
+            case "/":
+                return b / a;
+            case "*":
+                return b * a;
+            default:
+                return 0;
+        }
+    }
+
+    public static String[][] parseFile(String fileString) throws FileNotFoundException, IOException
+    {
+        String[][] numbers = new String[3][3];
+        File file = new File(fileString);
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line = "";
+        int rowNumber = 0;
+        int columnNumber = 0;
+        while ((line = bufferedReader.readLine()) != null)
+        {
+            String[] itemsLine;
+            itemsLine = line.split(",");
+            for (String item : itemsLine)
+            {
+                if (item != null)
+                {
+                    numbers[rowNumber][columnNumber] = item;
+                    columnNumber++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            rowNumber++;
+            columnNumber = 0;
+        }
+        bufferedReader.close();
+        return numbers;
     }
 
     // For Testing Purposes
